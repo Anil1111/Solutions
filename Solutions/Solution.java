fd6c27ee4b5771ba579942bc63c96406ac662e3c
Tony Wang <timts@users.noreply.github.com>
2014-03-13 03:10:50 -0500

    Update Solution.java

--- Solutions/Solution.java
+++ Solutions/Solution.java
@@ -30,15 +30,6 @@ public class Solution {
         return A[0];
     }
     
-    /**
-     * Definition for binary tree
-     * public class TreeNode {
-     *     int val;
-     *     TreeNode left;
-     *     TreeNode right;
-     *     TreeNode(int x) { val = x; }
-     * }
-     */
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == null && q == null) return true;
         else if (p != null && q != null) 
