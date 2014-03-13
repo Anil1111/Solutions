32f05574bbb1ae65e244c3ddee82e1809e2811c3
Tony Wang <timts@users.noreply.github.com>
2014-03-12 20:50:20 -0500

    Update Solution.java

--- Solutions/Solution.java
+++ Solutions/Solution.java
@@ -22,6 +22,12 @@ public class Solution {
         return true;
     }
     
-    
+    public int singleNumber(int[] A) {
+        if (A.length==0) return 0;
+        for(int i=1;i<A.length;i++){
+            A[0] ^= A[i];
+        }
+        return A[0];
+    }
     
 }
