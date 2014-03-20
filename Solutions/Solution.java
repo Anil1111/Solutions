2c6af41145e08945d381011538bcbf0e848c11bd
Tony Wang <timts@users.noreply.github.com>
2014-03-20 06:26:31 -0500

    Update Solution.java

--- Solutions/Solution.java
+++ Solutions/Solution.java
@@ -1309,46 +1309,53 @@ public class Solution {
     }
     
     public ArrayList<ArrayList<Integer>> fourSum(int[] num, int target) {
-     	Arrays.sort(num);
-     
-    	HashSet<ArrayList<Integer>> hashSet = new HashSet<ArrayList<Integer>>();
-    	ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-     
-    	for (int i = 0; i < num.length; i++) {
-    		for (int j = i + 1; j < num.length; j++) {
-    			int k = j + 1;
-    			int l = num.length - 1;
-     
-    			while (k < l) {
-    				int sum = num[i] + num[j] + num[k] + num[l];
-     
-    				if (sum > target) {
-    					l--;
-    				} else if (sum < target) {
-    					k++;
-    				} else if (sum == target) {
-    					ArrayList<Integer> temp = new ArrayList<Integer>();
-    					temp.add(num[i]);
-    					temp.add(num[j]);
-    					temp.add(num[k]);
-    					temp.add(num[l]);
-     
-    					if (!hashSet.contains(temp)) {
-    						hashSet.add(temp);
-    						result.add(temp);
-    					}
-     
-    					k++;
-    					l--;
-    				}
-    			}
-    		}
-    	}
-     
-    	return result;
-        }
+	 	Arrays.sort(num);
+	 
+		HashSet<ArrayList<Integer>> hashSet = new HashSet<ArrayList<Integer>>();
+		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+	 
+		for (int i = 0; i < num.length; i++) {
+			for (int j = i + 1; j < num.length; j++) {
+				int k = j + 1;
+				int l = num.length - 1;
+	 
+				while (k < l) {
+					int sum = num[i] + num[j] + num[k] + num[l];
+	 
+					if (sum > target) {
+						l--;
+					} else if (sum < target) {
+						k++;
+					} else if (sum == target) {
+						ArrayList<Integer> temp = new ArrayList<Integer>();
+						temp.add(num[i]);
+						temp.add(num[j]);
+						temp.add(num[k]);
+						temp.add(num[l]);
+	 
+						if (!hashSet.contains(temp)) {
+							hashSet.add(temp);
+							result.add(temp);
+						}
+	 
+						k++;
+						l--;
+					}
+				}
+			}
+		}
+	 
+		return result;
+	}
         
-        
+    public int removeElement(int[] A, int elem) {
+      int offset = 0;
+      for(int i=0;i<A.length;i++){
+    	  if (A[i]==elem) offset ++;    	  
+    	  else if (offset > 0) A[i-offset]=A[i];
+      }
+      return A.length - offset;
+    }
         
 }
 
