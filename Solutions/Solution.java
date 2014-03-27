5f1bebd4672ed24921dadb931ca78f0426745ecc
Tony Wang <timts@users.noreply.github.com>
2014-03-26 21:25:07 -0500

    Update Solution.java

--- Solutions/Solution.java
+++ Solutions/Solution.java
@@ -2281,6 +2281,24 @@ public class Solution {
 		return ret;
     }
     
+    /**
+     * f(n)=f(n-1)+f(n-2) 1 or 2 steps
+     * @param n
+     * @return
+     */
+    public int climbStairs(int n) {
+        int prev = 0, cur = 1;
+        for(int i=0;i<n;i++){
+        	int temp = cur;
+        	cur += prev;
+        	prev = temp;
+        }
+        return cur;
+    }
+    
+    
+    
+    
     
 }
 
