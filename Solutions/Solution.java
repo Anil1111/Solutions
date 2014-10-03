761af204dbe2e7b08735bf690efc4aad278c8750
Tony Wang <timts@users.noreply.github.com>
2014-10-03 13:51:08 -0700

    maxproduct

--- Solutions/Solution.java
+++ Solutions/Solution.java
@@ -57,6 +57,7 @@ public class Solution {
 		//solution.test_longestConsecutive();
 		//solution.test_lengthOfLongestSubstring();
 		//solution.trest_ladderLength();
+		//solution.trest_findLadders();
 	}
 	   
     public int maxDepth(TreeNode root) {
@@ -3005,8 +3006,96 @@ public class Solution {
         return 0;
     }
     
+    public int maxProduct(int[] A) {
+        if (A.length == 0) return 0;
+        
+        int maxpre = A[0], minpre = A[0], maxhere, minhere, max=A[0];
+        for(int i=1;i<A.length;i++){
+        	maxhere = Math.max(Math.max(maxpre*A[i],  minpre*A[i]), A[i]);
+        	minhere = Math.min(Math.min(maxpre*A[i],  minpre*A[i]), A[i]);
+        	max = Math.max(maxhere, max);
+        	maxpre = maxhere;
+        	minpre = minhere;
+        }
+        
+        return max;
+    }
     
+    public void trest_findLadders(){
+    	HashSet<String> dict = new HashSet<String>();
+    	dict.add("a");
+    	dict.add("b");
+    	dict.add("c");
+    	List<List<String>> ret = new ArrayList<List<String>>();
+    	//ret = findLadders("a", "c", dict);
+    	for(List<String> list : ret){
+    		for(String word : list){
+    			System.out.printf("%s ", word);
+    		}
+    		System.out.println();
+    	}
+    	
+    	//Input: 	"hot", "dog", ["hot","cog","dog","tot","hog","hop","pot","dot"]
+    	//Expected: ["hot","dot","dog"],["hot","hog","dog"]]   	
+    	dict = new HashSet<String>();
+    	dict.add("hot");
+    	dict.add("cog");
+    	dict.add("dog");
+    	dict.add("tot");
+    	dict.add("hog");
+    	dict.add("hop");
+    	dict.add("pot");
+    	dict.add("dot");
+    	//ret = findLadders("hot", "dog", dict);
+    	for(List<String> list : ret){
+    		for(String word : list){
+    			System.out.printf("%s ", word);
+    		}
+    		System.out.println();
+    	}
+    	
+    	//Input: 	"red", "tax", ["ted","tex","red","tax","tad","den","rex","pee"]
+    	//Expected: 	[["red","ted","tad","tax"],["red","ted","tex","tax"],["red","rex","tex","tax"]]
+    	dict = new HashSet<String>();
+    	dict.add("ted");
+    	dict.add("tex");
+    	dict.add("red");
+    	dict.add("tax");
+    	dict.add("tad");
+    	dict.add("den");
+    	dict.add("rex");
+    	dict.add("pee");
+    	//ret = findLadders("red", "tax", dict);
+    	for(List<String> list : ret){
+    		for(String word : list){
+    			System.out.printf("%s ", word);
+    		}
+    		System.out.println();
+    	}
+    	
+    	//timeout
+    	//Last executed input: 	"cet", "ism", 
+    	dict = new HashSet<String>();
+    	for(String word : new String[]{"kid","tag","pup","ail","tun","woo","erg","luz","brr","gay","sip","kay","per","val","mes","ohs","now","boa","cet","pal","bar","die","war","hay","eco","pub","lob","rue","fry","lit","rex","jan","cot","bid","ali","pay","col","gum","ger","row","won","dan","rum","fad","tut","sag","yip","sui","ark","has","zip","fez","own","ump","dis","ads","max","jaw","out","btu","ana","gap","cry","led","abe","box","ore","pig","fie","toy","fat","cal","lie","noh","sew","ono","tam","flu","mgm","ply","awe","pry","tit","tie","yet","too","tax","jim","san","pan","map","ski","ova","wed","non","wac","nut","why","bye","lye","oct","old","fin","feb","chi","sap","owl","log","tod","dot","bow","fob","for","joe","ivy","fan","age","fax","hip","jib","mel","hus","sob","ifs","tab","ara","dab","jag","jar","arm","lot","tom","sax","tex","yum","pei","wen","wry","ire","irk","far","mew","wit","doe","gas","rte","ian","pot","ask","wag","hag","amy","nag","ron","soy","gin","don","tug","fay","vic","boo","nam","ave","buy","sop","but","orb","fen","paw","his","sub","bob","yea","oft","inn","rod","yam","pew","web","hod","hun","gyp","wei","wis","rob","gad","pie","mon","dog","bib","rub","ere","dig","era","cat","fox","bee","mod","day","apr","vie","nev","jam","pam","new","aye","ani","and","ibm","yap","can","pyx","tar","kin","fog","hum","pip","cup","dye","lyx","jog","nun","par","wan","fey","bus","oak","bad","ats","set","qom","vat","eat","pus","rev","axe","ion","six","ila","lao","mom","mas","pro","few","opt","poe","art","ash","oar","cap","lop","may","shy","rid","bat","sum","rim","fee","bmw","sky","maj","hue","thy","ava","rap","den","fla","auk","cox","ibo","hey","saw","vim","sec","ltd","you","its","tat","dew","eva","tog","ram","let","see","zit","maw","nix","ate","gig","rep","owe","ind","hog","eve","sam","zoo","any","dow","cod","bed","vet","ham","sis","hex","via","fir","nod","mao","aug","mum","hoe","bah","hal","keg","hew","zed","tow","gog","ass","dem","who","bet","gos","son","ear","spy","kit","boy","due","sen","oaf","mix","hep","fur","ada","bin","nil","mia","ewe","hit","fix","sad","rib","eye","hop","haw","wax","mid","tad","ken","wad","rye","pap","bog","gut","ito","woe","our","ado","sin","mad","ray","hon","roy","dip","hen","iva","lug","asp","hui","yak","bay","poi","yep","bun","try","lad","elm","nat","wyo","gym","dug","toe","dee","wig","sly","rip","geo","cog","pas","zen","odd","nan","lay","pod","fit","hem","joy","bum","rio","yon","dec","leg","put","sue","dim","pet","yaw","nub","bit","bur","sid","sun","oil","red","doc","moe","caw","eel","dix","cub","end","gem","off","yew","hug","pop","tub","sgt","lid","pun","ton","sol","din","yup","jab","pea","bug","gag","mil","jig","hub","low","did","tin","get","gte","sox","lei","mig","fig","lon","use","ban","flo","nov","jut","bag","mir","sty","lap","two","ins","con","ant","net","tux","ode","stu","mug","cad","nap","gun","fop","tot","sow","sal","sic","ted","wot","del","imp","cob","way","ann","tan","mci","job","wet","ism","err","him","all","pad","hah","hie","aim","ike","jed","ego","mac","baa","min","com","ill","was","cab","ago","ina","big","ilk","gal","tap","duh","ola","ran","lab","top","gob","hot","ora","tia","kip","han","met","hut","she","sac","fed","goo","tee","ell","not","act","gil","rut","ala","ape","rig","cid","god","duo","lin","aid","gel","awl","lag","elf","liz","ref","aha","fib","oho","tho","her","nor","ace","adz","fun","ned","coo","win","tao","coy","van","man","pit","guy","foe","hid","mai","sup","jay","hob","mow","jot","are","pol","arc","lax","aft","alb","len","air","pug","pox","vow","got","meg","zoe","amp","ale","bud","gee","pin","dun","pat","ten","mob"}){
+    		dict.add(word);
+    	}
+    	ret = findLadders("cet", "ism", dict);
+    	for(List<String> list : ret){
+    		for(String word : list){
+    			System.out.printf("%s ", word);
+    		}
+    		System.out.println();
+    	}
+    }
     
+    public List<List<String>> findLadders(String start, String end, Set<String> dict) {
+    	List<List<String>> ret = new ArrayList<List<String>>();
+
+        
+    	return ret;
+    }
+    
+
     
 }
 
