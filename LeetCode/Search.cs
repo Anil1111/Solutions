bacf4fb46f42f6b9f5f26f63e4bc26d53360725a
Tony Wang <timts@users.noreply.github.com>
2015-03-19 16:45:40 -0700

    81 	Search in Rotated Sorted Array II

--- LeetCode/Search.cs
+++ LeetCode/Search.cs
@@ -1,9 +1,18 @@
 public class Solution {
     public bool Search(int[] A, int target) {
-        for(int i=0;i<A.Length;i++){
-            if (A[i]==target) return true;
+        int l=0,r=A.Length-1;
+        while(l<r){
+            int m = (l+r)/2;
+            if (A[m]==target) return true;
+            if (A[l] < A[m]) { //left half is sorted
+                if (A[l] <= target && target < A[m]) r = m - 1;
+                else l = m + 1;
+            } else if (A[l] > A[m]) { //right half is sorted
+                if (A[m] < target && target <= A[r]) l = m + 1;
+                else r = m - 1;
+            } else l++;
         }
-
-        return false;
+        
+        return A[l]==target;
     }
 }
