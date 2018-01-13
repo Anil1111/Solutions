bca267091bfd571b698e402fc2ec5d887ae0c38c
Tony Wang <timts@users.noreply.github.com>
2018-01-13 10:36:32 -0800

    760. Find Anagram Mappings
    add duplicate handling

--- LeetCode/760AnagramMappings.cs
+++ LeetCode/760AnagramMappings.cs
@@ -1,9 +1,21 @@
 public class Solution {
     public int[] AnagramMappings(int[] A, int[] B) {
         var list = new List<int>();
-        var dict = new Dictionary<int, int>();
-        for(int i=0;i<B.Length;i++) dict[B[i]] = i;
-        for(int i=0;i<A.Length;i++) list.Add(dict[A[i]]);
+        var dict = new Dictionary<int, List<int>>();
+        for(int i=0;i<B.Length;i++){ 
+            List<int> pos;
+            if (!dict.TryGetValue(B[i], out pos)){
+                pos = new List<int>();
+            }
+            pos.Add(i);
+            dict[B[i]]=pos;
+        }
+        for(int i=0;i<A.Length;i++){
+            var pos = dict[A[i]];
+            var val = pos[0];
+            pos.Remove(val);
+            list.Add(val);  
+        } 
         return list.ToArray();
     }
 }
