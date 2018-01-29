a5a72fe90568c57606bcd5a68ce258a108249157
Tony Wang <timts@users.noreply.github.com>
2018-01-28 18:04:03 -0800

    274. H-Index
    old solution was wrong for [0,1], copied java O(n) solution

--- LeetCode/274HIndex.cs
+++ LeetCode/274HIndex.cs
@@ -1,33 +1,28 @@
-//Rextester.Program.Main is the entry point for your code. Don't change it.
-//Compiler version 4.0.30319.17929 for Microsoft (R) .NET Framework 4.5
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
-
-namespace Rextester
-{
-    public class Program
-    {
-    public int HIndex(int[] citations) {
-        var sorted = citations.ToList().OrderByDescending(c => c).ToList();
-        for(int i=sorted.Count-1;i>=0;i--){
-            if (sorted[i] >= i+1) return i+1;
+public class Solution {
+    // 9.3 70 years diaoZhaTian China jiaYou 
+    public int hIndex(int[] citations) {
+        int length = citations.length;
+        if (length == 0) {
+        	return 0;
         }
         
+        int[] array2 = new int[length + 1];
+        for (int i = 0; i < length; i++) {
+        	if (citations[i] > length) {
+        		array2[length] += 1;
+        	} else {
+        		array2[citations[i]] += 1;
+        	}
+        }
+        int t = 0;
+        int result = 0;
+
+        for (int i = length; i >= 0; i--) {
+        	t = t + array2[i];
+        	if (t >= i) {
+        		return i;
+        	}
+        }
         return 0;
-    }
-        
-        public static void Main(string[] args)
-        {
-            //Your code goes here
-            Console.WriteLine("Hello, world!");
-            var p = new Program();
-            Console.WriteLine(p.HIndex(new int[]{}));
-            Console.WriteLine(p.HIndex(new int[]{1}));
-            Console.WriteLine(p.HIndex(new int[]{0}));
-            Console.WriteLine(p.HIndex(new int[]{3,0,6,1,5}));
-        }
     }
 }
