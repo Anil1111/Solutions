f7106c7b454259b8ee45b6e7454e43b5911915d2
Tony Wang <timts@users.noreply.github.com>
2018-02-08 11:22:18 -0800

    215. Kth Largest Element in an Array
    copied https://leetcode.com/problems/kth-largest-element-in-an-array/discuss/60300/Java-Quick-Select

--- LeetCode/215FindKthLargest.cs
+++ LeetCode/215FindKthLargest.cs
@@ -1,6 +1,28 @@
 public class Solution {
     public int FindKthLargest(int[] nums, int k) {
-        Array.Sort(nums);
-        return nums[nums.Length-k];
+        int start = 0, end = nums.Length - 1, index = nums.Length - k;
+        while (start < end) {
+            int pivot = Partion(nums, start, end);
+            if (pivot < index) start = pivot + 1; 
+            else if (pivot > index) end = pivot - 1;
+            else return nums[pivot];
+        }
+        return nums[start];
+    }
+    
+    private int Partion(int[] nums, int start, int end) {
+        int pivot = start, temp;
+        while (start <= end) {
+            while (start <= end && nums[start] <= nums[pivot]) start++;
+            while (start <= end && nums[end] > nums[pivot]) end--;
+            if (start > end) break;
+            temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+        }
+        temp = nums[end];
+        nums[end] = nums[pivot];
+        nums[pivot] = temp;
+        return end;
     }
 }
