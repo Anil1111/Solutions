6469fa7694db947ef5a897514a729dc0697662b2
Tony Wang <timts@users.noreply.github.com>
2018-03-08 19:08:17 -0800

    365. Water and Jug Problem
    https://leetcode.com/problems/water-and-jug-problem/discuss/83716/Java-Programmatic-Solution-BFS-without-GCD

--- LeetCode/365CanMeasureWater.cs
+++ LeetCode/365CanMeasureWater.cs
@@ -1,72 +1,27 @@
-//Rextester.Program.Main is the entry point for your code. Don't change it.
-//Compiler version 4.0.30319.17929 for Microsoft (R) .NET Framework 4.5
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
-using System.Windows;
-
-namespace Rextester
-{
-    public class Program
-    {
-    public class Point{
-        public int X{get; set;}
-        public int Y{get; set;}
-        public Point(int x, int y){
-            X = x;
-            Y = y;
-        }
-    }
+public class Solution {
     public bool CanMeasureWater(int x, int y, int z) {
-        bool[,] dp = new bool[x+1,y+1];
-        
-        List<Point> list = new List<Point>();        
-        Add(dp, list, new Point(0,0));
-        Add(dp, list, new Point(x,0));
-        Add(dp, list, new Point(0,y));
-        Add(dp, list, new Point(x,y));
-        while(list.Count > 0){
-            var first = list.First();
-            list.RemoveAt(0);
-            int x1 = first.X;
-            int y1 = first.Y;
+        if (z < 0 || z > x + y) return false;
+        HashSet<int> set = new HashSet<int>();
+        List<int> q = new List<int>();
+        q.Add(0);
+        while (q.Count > 0) {
+            int n = q.First();
+            if (n == z) return true;
+            q.RemoveAt(0);
             
-            if (x1+y1 == z) return true;            
-            //Console.WriteLine(x1+" "+y1);
-            
-            if (!dp[0,y1]) Add(dp, list, new Point(0, y1));
-            if (!dp[x1,0]) Add(dp, list, new Point(x1, 0));
-            if (x1+y1 < x && !dp[x1+y1,0]) Add(dp, list, new Point(x1+y1,0));
-            if (x1+y1 < y && !dp[0, x1+y1]) Add(dp, list, new Point(0, x1+y1));
-            if (!dp[x,y1]) Add(dp, list, new Point(x, y1));
-            if (!dp[x1,y]) Add(dp, list, new Point(x1,y));
-
-            int y2 = y-y1;
-            int x2 = x-y2;
-            if (y2 > 0 && x2 > 0 && !dp[x2,y2]) Add(dp, list, new Point(x2,y2));
-            x2 = x-x1;
-            y2 = y1-x2;
-            if (x2 > 0 && y2 > 0 && !dp[x2,y2]) Add(dp, list, new Point(x2,y2));
+            if (n + x <= x + y && set.Add(n + x)) {
+                q.Add(n + x);
+            } 
+            if (n + y <= x + y && set.Add(n + y)) {
+                q.Add(n + y);
+            } 
+            if (n - x >= 0 && set.Add(n - x)) {
+                q.Add(n - x);
+            } 
+            if (n - y >= 0 && set.Add(n - y)) {
+                q.Add(n - y);
+            }
         }
-        
         return false;
-    }
-        
-        void Add(bool[,] dp, List<Point> list, Point point){
-            dp[point.X, point.Y] = true;
-            list.Add(point);
-        }
-        
-        public static void Main(string[] args)
-        {
-            //Your code goes here
-            Console.WriteLine("Hello, world!");
-            var p = new Program();
-            Console.WriteLine(p.CanMeasureWater(3,5,4));
-            Console.WriteLine(p.CanMeasureWater(2,6,5));
-            //Console.WriteLine(p.CanMeasureWater(22003,31237,1));
-        }
     }
 }
