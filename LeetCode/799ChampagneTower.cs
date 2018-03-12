4738e6ec67b55540a4ef2a3c960566003c418986
Tony Wang <timts@users.noreply.github.com>
2018-03-12 09:31:59 -0700

    799. Champagne Tower
    copied solution https://leetcode.com/articles/champagne-tower/

--- LeetCode/799ChampagneTower.cs
+++ LeetCode/799ChampagneTower.cs
@@ -1,44 +1,17 @@
-//Rextester.Program.Main is the entry point for your code. Don't change it.
-//Compiler version 4.0.30319.17929 for Microsoft (R) .NET Framework 4.5
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
-
-namespace Rextester
-{
-    public class Program
-    {
+public class Solution {
     public double ChampagneTower(int poured, int query_row, int query_glass) {
-        double p = poured;
-        for(int i=0;i<query_row;i++){
-            if (p <= 0) return 0;
-            if (p > (i+1)){
-                p = p - i - 1;
+        double[,] A = new double[102,102];
+        A[0,0] = (double) poured;
+        for (int r = 0; r <= query_row; ++r) {
+            for (int c = 0; c <= r; ++c) {
+                double q = (A[r,c] - 1.0) / 2.0;
+                if (q > 0) {
+                    A[r+1,c] += q;
+                    A[r+1,c+1] += q;
+                }
             }
-            else p = 0;
         }
-        Console.WriteLine(p);
-        if (p == 0) return 0;
-        else if (query_row == 0){
-            return p > 1 ? 1 : p;
-        }
-        else{
-            double count = (query_row - 1)*2 + 2;
-            Console.WriteLine(p+" "+count+" "+p/count);
-            if (query_glass == 0 || query_glass == query_row) return p / count > 1 ? 1 : p/count;
-            else return p * 2.0 / count > 1 ? 1 : p * 2.0 / count;
-        }
-    }
-        
-        public static void Main(string[] args)
-        {
-            //Your code goes here
-            Console.WriteLine("Hello, world!");
-            var p= new Program();
-            Console.WriteLine(p.ChampagneTower(6,3,1));
-            //Console.WriteLine(p.ChampagneTower(2,1,1));
-        }
+
+        return Math.Min(1, A[query_row,query_glass]);
     }
 }
