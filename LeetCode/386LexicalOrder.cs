971d69216c6fd26e5b3cb88fc80ab14e0ac055bb
Tony Wang <timts@users.noreply.github.com>
2018-04-06 07:28:02 -0700

    386. Lexicographical Numbers
    https://leetcode.com/problems/lexicographical-numbers/discuss/86242/Java-O(n)-time-O(1)-space-iterative-solution-130ms

---
+++ LeetCode/386LexicalOrder.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public IList<int> LexicalOrder(int n) {
+        List<int> list = new List<int>();
+        int curr = 1;
+        for (int i = 1; i <= n; i++) {
+            list.Add(curr);
+            if (curr * 10 <= n) {
+                curr *= 10;
+            } else if (curr % 10 != 9 && curr + 1 <= n) {
+                curr++;
+            } else {
+                while ((curr / 10) % 10 == 9) {
+                    curr /= 10;
+                }
+                curr = curr / 10 + 1;
+            }
+        }
+        return list;
+    }
+}
