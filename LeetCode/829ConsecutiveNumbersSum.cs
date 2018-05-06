4cc4b23bf9163e78944f8fec3dd42d9e27161f32
Tony Wang <timts@users.noreply.github.com>
2018-05-05 23:59:41 -0700

    829. Consecutive Numbers Sum
    https://leetcode.com/problems/consecutive-numbers-sum/solution/

--- LeetCode/829ConsecutiveNumbersSum.cs
+++ LeetCode/829ConsecutiveNumbersSum.cs
@@ -1,11 +1,19 @@
 public class Solution {
     public int ConsecutiveNumbersSum(int N) {
-        if (N==1) return 1;
-        
-        int count = 1 + N%2;
-        for(int i=3;i<=N/2;i+=2){
-            if (N%i==0) count++;
+        while ((N & 1) == 0) N >>= 1;
+        int ans = 1, d = 3;
+
+        while (d * d <= N) {
+            int e = 0;
+            while (N % d == 0) {
+                N /= d;
+                e++;
+            }
+            ans *= e + 1;
+            d += 2;
         }
-        return count;
+
+        if (N > 1) ans <<= 1;
+        return ans;
     }
 }
